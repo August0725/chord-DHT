dfbf15a5af8491449a8a954d11dcbc6c1e46544d
August0725 <sixuesnow@gmail.com>
2023-11-27 17:02:59 -0800

    modify function to find predecessor and successor

--- node.py
+++ node.py
@@ -86,40 +86,95 @@ class Node(pb2_grpc.ChordServicer):
         self.finger_table[next_idx][1] = succ
         return (next_idx + 1) % util.m
 
+    def check_predecessor(self):
+        while not self.stop_event.is_set():
+            time.sleep(1)
+            self._check_predecessor()
+
+    def _check_predecessor(self):
+        if self.predecessor is None:
+            return
+        if not self.rpc_client.check_predecessor(self.predecessor):
+            self.predecessor = None
+
     def find_successor(self, hid):
         pred = self.find_predecessor(hid)
+        if pred is None:
+            return None
         succ = self.get_successor_rpc(pred)
+        if succ is None:
+            return None
         return succ
 
     def find_predecessor(self, hid):
         curr = self.node
-        while not util.between_include_right(hid, curr.id, curr.successor.id):
+        succ = self.get_successor_rpc(curr)
+        if succ is None:
+            return None
+        while not util.between_include_right(hid, curr.id, succ.id):
             curr = self.closest_preceding_finger_rpc(curr, hid)
+            if curr is None:
+                return None
+            succ = self.get_successor_rpc(curr)
+            if succ is None:
+                return None
         return curr
 
     def closest_preceding_finger_rpc(self, node, hid):
-        return self.rpcClient.closest_preceding_finger(node, hid)
+        return self.rpc_client.closest_preceding_finger(node, hid)
 
     def get_successor_rpc(self, node):
-        return self.rpcClient.get_successor(node)
+        return self.rpc_client.get_successor(node)
 
     def find_successor_rpc(self, node, hid):
-        return self.rpcClient.find_successor(node, hid)
+        return self.rpc_client.find_successor(node, hid)
+
+    def get_predecessor_rpc(self, node):
+        return self.rpc_client.get_predecessor(node)
+
+    def notify_rpc(self, node, pred):
+        return self.rpc_client.notify(node, pred)
+
+    def get_kv_rpc(self, node, key):
+        return self.rpc_client.get_kv(node, key)
+
+    def set_kv_rpc(self, node, kv):
+        return self.rpc_client.set_kv(node, kv)
+
+    def delete_kv_rpc(self, node, key):
+        return self.rpc_client.delete_kv(node, key)
+
+    def find_kvs_rpc(self):
+        return self.rpc_client.find_kvs(self.node, self.successor)
+
+    def set_successor_rpc(self, node, succ):
+        return self.rpc_client.set_successor(node, succ)
+
+    def set_predecessor_rpc(self, node, pred):
+        return self.rpc_client.set_predecessor(node, pred)
+
+    def move_data_rpc(self, kvs):
+        return self.rpc_client.move_data(self.successor, kvs)
 
     def GetPredecessor(self, request, context):
+        if self.predecessor is None:
+            return pb2.Node(id=-1, addr="")
         return self.predecessor
 
     def GetSuccessor(self, request, context):
         return self.successor
 
     def FindSuccessor(self, request, context):
-        hid = request.ID
-        return self.find_successor(hid)
+        hid = request.id
+        res = self.find_successor(hid)
+        if res is None:
+            return pb2.Node(id=-1, addr="")
+        return res
 
     def ClosestPrecedingFinger(self, request, context):
-        hid = request.ID
+        hid = request.id
         for i in range(len(self.finger_table) - 1, -1, -1):
-            if util.between(self.finger_table[i][1].id, self.node.id, hid):
+            if self.finger_table[i][1] is not None and util.between(self.finger_table[i][1].id, self.node.id, hid):
                 return self.finger_table[i][1]
         return self.node
 
